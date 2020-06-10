1dfb531bea874fab99dc00041bce6735a9d83443
QB3 <quentin.bertrand@inria.fr>
2020-06-10 17:28:40 +0200

    ENH added file for gradient descent fig

--- expes/expe_fig_cross_val/plot.py
+++ expes/expe_fig_cross_val/plot.py
@@ -16,6 +16,9 @@ current_palette = sns.color_palette("colorblind")
 p_alphas = np.load("p_alphas.npy")
 objs = np.load("objs.npy")
 
+p_alphas_grad = np.load("p_alphas_grad.npy")
+objs_grad = np.load("objs_grad.npy")
+
 # ax = plt.gca()
 # ax.tick_params(width=10)
 
@@ -23,8 +26,11 @@ fig = plt.figure()
 plt.semilogx(
     p_alphas, objs, color=current_palette[0], linewidth=7.0)
 plt.semilogx(
-    p_alphas, objs, 'bo', label='grid-search',
+    p_alphas, objs, 'bo', label='0-order method (grid-search)',
     color=current_palette[1])
+# plt.semilogx(
+#     p_alphas_grad, objs_grad, 'bX', label='1rst order method',
+#     color=current_palette[2])
 plt.xlabel(r"$\lambda / \lambda_{\max}$", fontsize=28)
 plt.ylabel("validation loss", fontsize=28)
 plt.tick_params(width=5)
@@ -34,6 +40,31 @@ plt.tight_layout()
 if save_fig:
     fig.savefig(
         fig_dir + "cross_val_real_sim.pdf", bbox_inches="tight")
+fig.show()
+
+plt.show(block=False)
+
+
+# plot fig with 0 order and 1rst order methods
+
+fig = plt.figure()
+plt.semilogx(
+    p_alphas, objs, color=current_palette[0], linewidth=7.0)
+plt.semilogx(
+    p_alphas, objs, 'bo', label='0-order method (grid-search)',
+    color=current_palette[1])
+plt.semilogx(
+    p_alphas_grad, objs_grad, 'bX', label='1-rst order method',
+    color=current_palette[2])
+plt.xlabel(r"$\lambda / \lambda_{\max}$", fontsize=28)
+plt.ylabel("validation loss", fontsize=28)
+plt.tick_params(width=5)
+plt.legend(fontsize=28)
+plt.tight_layout()
+
+if save_fig:
+    fig.savefig(
+        fig_dir + "cross_val_and_grad_search_real_sim.pdf", bbox_inches="tight")
 fig.show()
 
 plt.show(block=False)
