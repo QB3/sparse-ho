0ce54bf787727106fe714f23497e896862a60685
QB3 <quentin.bertrand@inria.fr>
2020-06-17 21:53:53 +0200

    EXP modified plotting file

--- expes/expe_linear_convergence/plot.py
+++ expes/expe_linear_convergence/plot.py
@@ -19,6 +19,9 @@ fontsize = 25
 # dataset_names = ["leu", "rcv1_train", "news20"]
 # dataset_names = ["leu", "rcv1_train", "news20", "finance"]
 dataset_names = ["leu", "rcv1_train", "news20", "real-sim"]
+# model_names = ["lasso"]
+model_names = ["lasso", "logreg"]
+
 
 dict_title = {}
 dict_title["rcv1_train"] = "rcv1"
@@ -29,38 +32,42 @@ dict_title["climate"] = "climate"
 dict_title["leu"] = "Leukemia"
 dict_title["real-sim"] = "real-sim"
 
-plt.close('all')
-fig, axarr = plt.subplots(
-    2, 4, sharex=False, sharey=False, figsize=[14, 8],)
+for model_name in model_names:
+    # plt.close('all')
+    fig, axarr = plt.subplots(
+        2, 4, sharex=False, sharey =False, figsize=[14, 8],)
+    for idx, dataset in enumerate(dataset_names):
+        try:
+            df_data = pandas.read_pickle(
+                "%s_%s.pkl" % (dataset, model_name))
+        except:
+            break
+        diff_beta = df_data["diff_beta"].to_numpy()[0]
+        diff_jac = df_data["diff_jac"].to_numpy()[0]
+        supp_id = df_data["supp_id"].to_numpy()[0]
+        #
+        axarr.flat[idx].semilogy(diff_beta)
+        axarr.flat[idx].axvline(x=supp_id, c='red', linestyle="--")
 
+        axarr.flat[idx+4].semilogy(diff_jac)
+        axarr.flat[idx+4].axvline(x=supp_id, c='red', linestyle="--")
 
-for idx, dataset in enumerate(dataset_names):
-    df_data = pandas.read_pickle("%s.pkl" % dataset)
-    diff_beta = df_data["diff_beta"].to_numpy()[0]
-    diff_jac = df_data["diff_jac"].to_numpy()[0]
-    supp_id = df_data["supp_id"].to_numpy()[0]
-    #
-    axarr.flat[idx].semilogy(diff_beta)
-    axarr.flat[idx].axvline(x=supp_id, c='red', linestyle="--")
+        axarr.flat[idx+4].set_xlabel(r"$\#$ epochs")
 
-    axarr.flat[idx+4].semilogy(diff_jac)
-    axarr.flat[idx+4].axvline(x=supp_id, c='red', linestyle="--")
+        axarr.flat[idx].set_title("%s" % (
+            dict_title[dataset]), size=fontsize)
+        # xarr.flat[idx].set_title("%s %s" % (
+        #     dict_title[dataset], dict_n_feature[dataset]), size=fontsize)
 
-    axarr.flat[idx+4].set_xlabel(r"$\#$ epochs")
+    axarr.flat[0].set_ylabel(r"$||\beta^{(k)} - \hat \beta||$")
+    axarr.flat[4].set_ylabel(r"$||\mathcal{J}^{(k)} - \hat \mathcal{J}||$")
 
-    axarr.flat[idx].set_title("%s" % (
-        dict_title[dataset]), size=fontsize)
-    # xarr.flat[idx].set_title("%s %s" % (
-    #     dict_title[dataset], dict_n_feature[dataset]), size=fontsize)
+    fig.tight_layout()
 
-axarr.flat[0].set_ylabel(r"$||\beta^{(k)} - \hat \beta||$")
-axarr.flat[4].set_ylabel(r"$||\mathcal{J}^{(k)} - \hat \mathcal{J}||$")
-
-fig.tight_layout()
-
-if save_fig:
-    fig.savefig(
-        fig_dir + "linear_convergence_lasso.pdf", bbox_inches="tight")
-    fig.savefig(
-        fig_dir_svg + "linear_convergence_lasso.svg", bbox_inches="tight")
-fig.show()
+    if save_fig:
+        fig.savefig(
+            fig_dir + "linear_convergence_lasso.pdf", bbox_inches="tight")
+        fig.savefig(
+            fig_dir_svg + "linear_convergence_lasso.svg", bbox_inches="tight")
+    # plt.title(model_name)
+    fig.show()
