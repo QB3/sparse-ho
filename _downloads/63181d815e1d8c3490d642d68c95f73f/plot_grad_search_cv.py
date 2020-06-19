44e7733f04e76072d371ff14b7fb3e3e4f5fd85a
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2020-06-19 17:01:43 +0100

    Generate site

---
+++ _downloads/63181d815e1d8c3490d642d68c95f73f/plot_grad_search_cv.py
@@ -0,0 +1,89 @@
+"""
+======================
+Grad Search CV
+======================
+
+...
+
+"""
+
+# Authors: Quentin Bertrand <quentin.bertrand@inria.fr>
+#          Quentin Klopfenstein <quentin.klopfenstein@u-bourgogne.fr>
+#
+# License: BSD (3-clause)
+
+import time
+import numpy as np
+# from numpy.linalg import norm
+
+# from sklearn.datasets import make_regression
+# from sklearn import linear_model
+from sklearn.linear_model import LassoCV
+from sparse_ho.models import Lasso
+from sparse_ho.criterion import CV
+from sparse_ho.implicit_forward import ImplicitForward
+from sparse_ho.utils import Monitor
+from sparse_ho.grad_search_CV import grad_search_CV
+# from sparse_ho.datasets.real import load_libsvm
+from sklearn.datasets import make_regression
+
+print(__doc__)
+
+# X, y = load_libsvm('real-sim')
+X, y = make_regression(
+    n_samples=2000, n_features=1000)
+
+random_state = 0
+cv = 5
+
+print("Starting path computation...")
+n_samples = len(y)
+alpha_max = np.max(np.abs(X.T.dot(y))) / n_samples
+
+n_alphas = 100
+alphas = alpha_max * np.geomspace(1, 0.00001, n_alphas)
+
+tol = 1e-8
+
+print('scikit started')
+
+t0 = time.time()
+reg = LassoCV(
+    cv=cv, random_state=random_state, verbose=True, tol=tol, fit_intercept=False, alphas=alphas).fit(X, y)
+reg.score(X, y)
+t_sk = time.time() - t0
+
+print('scikit finished')
+
+
+print('sparse-ho started')
+
+t0 = time.time()
+Model = Lasso
+Criterion = CV
+Algo = ImplicitForward
+log_alpha0 = np.log(alpha_max/10)
+monitor = Monitor()
+grad_search_CV(
+    X, y, Model, Criterion, Algo, log_alpha0, monitor, n_outer=100,
+    verbose=True, cv=cv, random_state=0, test_size=0.33,
+    tolerance_decrease='constant', tol=tol,
+    t_max=1000)
+t_grad_search = time.time() - t0
+
+print('sparse-ho finished')
+print("Time to compute CV for scikit-learn: %.2f" % t_sk)
+print("Time to compute CV for sparse-ho: %.2f" % t_grad_search)
+
+# clf = linear_model.Lasso(alpha=reg.alpha_)
+# clf.fit(X, y)
+# print(
+#     norm(X @ clf.coef_ - y) ** 2 / (2 * n_samples) + reg.alpha_ * norm(
+#         clf.coef_, ord=1))
+
+# clf2 = linear_model.Lasso(alpha=np.exp(monitor.log_alphas[-1]))
+# clf2.fit(X, y)
+
+# print(
+#     norm(X @ clf2.coef_ - y) ** 2 / (2 * n_samples) + np.exp(
+#         monitor.log_alphas[-1]) * norm(clf2.coef_, ord=1))
---
+++ _downloads/a9588e66a2dbadd4c004d1e34b56758d/plot_sparse_log_reg.py
@@ -0,0 +1,77 @@
+"""
+===========================
+Method comparison on Lasso
+===========================
+
+The aim of this example is to demonstrate on a simple
+dateset how methods compare.
+
+"""
+
+# Authors: Quentin Bertrand <quentin.bertrand@inria.fr>
+#          Quentin Klopfenstein <quentin.klopfenstein@u-bourgogne.fr>
+#
+# License: BSD (3-clause)
+
+
+import numpy as np
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+from sklearn import datasets
+from scipy.sparse import csc_matrix
+
+from sparse_ho.ho import grad_search
+from sparse_ho.utils import Monitor
+from sparse_ho.models import SparseLogreg
+from sparse_ho.criterion import Logistic
+from sparse_ho.implicit_forward import ImplicitForward
+from sparse_ho.forward import Forward
+from sparse_ho.grid_search import grid_search
+# from sparse_ho.bayesian import hyperopt_lasso
+
+from sparse_ho.datasets.real import get_rcv1
+# from expes.utils import configure_plt
+
+print(__doc__)
+
+X_train, X_val, X_test, y_train, y_val, y_test = get_rcv1()
+
+n_samples = 100
+n_features = 1000
+X_train, y_train = datasets.make_classification(
+    n_samples=n_samples,
+    n_features=n_features, n_informative=50,
+    random_state=10, flip_y=0.1, n_redundant=0)
+X_train_s = csc_matrix(X_train)
+
+
+X_val, y_val = datasets.make_classification(
+    n_samples=n_samples,
+    n_features=n_features, n_informative=50,
+    random_state=12, flip_y=0.1, n_redundant=0)
+
+# X_val_s = csc_matrix(X_val)
+
+
+n_samples, n_features = X_train.shape
+
+alpha_max = np.max(np.abs(X_train.T @ y_train))
+alpha_max /= 2 * n_samples
+log_alpha_max = np.log(alpha_max)
+log_alpha_min = np.log(alpha_max / 100)
+maxit = 1000
+
+log_alpha0 = np.log(0.3 * alpha_max)
+tol = 1e-2
+
+model = SparseLogreg(X_train, y_train, log_alpha0, max_iter=10000, tol=tol)
+criterion = Logistic(X_val, y_val, model)
+monitor = Monitor()
+algo = ImplicitForward(criterion, tol_jac=tol, n_iter_jac=100)
+grad_search(algo, log_alpha0, monitor, n_outer=3, tol=tol)
+
+
+monitor_grid = Monitor()
+algo_grid = Forward(criterion)
+grid_search(
+    algo_grid, log_alpha_min, log_alpha_max, monitor_grid, max_evals=5)
