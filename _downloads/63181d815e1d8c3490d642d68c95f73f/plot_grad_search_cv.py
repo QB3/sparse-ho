62b172449dd93362149bb28a786154c5de769a3d
QB3 <quentin.bertrand@inria.fr>
2020-07-10 10:28:17 +0200

    Generate site

--- _downloads/63181d815e1d8c3490d642d68c95f73f/plot_grad_search_cv.py
+++ _downloads/63181d815e1d8c3490d642d68c95f73f/plot_grad_search_cv.py
@@ -1,7 +1,7 @@
 """
-======================
-Grad Search CV
-======================
+=============================
+Example with cross validation
+=============================
 
 ...
 
@@ -14,7 +14,8 @@ Grad Search CV
 
 import time
 import numpy as np
-# from numpy.linalg import norm
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # from sklearn.datasets import make_regression
 # from sklearn import linear_model
@@ -24,24 +25,28 @@ from sparse_ho.criterion import CV
 from sparse_ho.implicit_forward import ImplicitForward
 from sparse_ho.utils import Monitor
 from sparse_ho.grad_search_CV import grad_search_CV
-# from sparse_ho.datasets.real import load_libsvm
-from sklearn.datasets import make_regression
+from sparse_ho.datasets.real import load_libsvm
+
+from sklearn.model_selection import KFold
 
 print(__doc__)
 
-# X, y = load_libsvm('real-sim')
-X, y = make_regression(
-    n_samples=2000, n_features=1000)
+X, y = load_libsvm('rcv1_train')
+# X, y = make_regression(
+#     n_samples=2000, n_features=1000)
 
-random_state = 0
-cv = 5
+kf = KFold(n_splits=5, shuffle=False)
+
+for train, test in kf.split(X):
+    print("%s %s" % (train, test))
 
 print("Starting path computation...")
 n_samples = len(y)
 alpha_max = np.max(np.abs(X.T.dot(y))) / n_samples
 
-n_alphas = 100
-alphas = alpha_max * np.geomspace(1, 0.00001, n_alphas)
+n_alphas = 10
+p_alphas = np.geomspace(1, 0.0001, n_alphas)
+alphas = alpha_max * p_alphas
 
 tol = 1e-8
 
@@ -49,7 +54,7 @@ print('scikit started')
 
 t0 = time.time()
 reg = LassoCV(
-    cv=cv, random_state=random_state, verbose=True, tol=tol, fit_intercept=False, alphas=alphas).fit(X, y)
+    cv=kf, verbose=True, tol=tol, fit_intercept=False, alphas=alphas).fit(X, y)
 reg.score(X, y)
 t_sk = time.time() - t0
 
@@ -65,8 +70,8 @@ Algo = ImplicitForward
 log_alpha0 = np.log(alpha_max/10)
 monitor = Monitor()
 grad_search_CV(
-    X, y, Model, Criterion, Algo, log_alpha0, monitor, n_outer=100,
-    verbose=True, cv=cv, random_state=0, test_size=0.33,
+    X, y, Model, Criterion, Algo, log_alpha0, monitor, n_outer=10,
+    verbose=True, cv=kf, random_state=0, test_size=0.33,
     tolerance_decrease='constant', tol=tol,
     t_max=1000)
 t_grad_search = time.time() - t0
@@ -75,15 +80,31 @@ print('sparse-ho finished')
 print("Time to compute CV for scikit-learn: %.2f" % t_sk)
 print("Time to compute CV for sparse-ho: %.2f" % t_grad_search)
 
-# clf = linear_model.Lasso(alpha=reg.alpha_)
-# clf.fit(X, y)
-# print(
-#     norm(X @ clf.coef_ - y) ** 2 / (2 * n_samples) + reg.alpha_ * norm(
-#         clf.coef_, ord=1))
 
-# clf2 = linear_model.Lasso(alpha=np.exp(monitor.log_alphas[-1]))
-# clf2.fit(X, y)
+objs = reg.mse_path_.mean(axis=1)
 
-# print(
-#     norm(X @ clf2.coef_ - y) ** 2 / (2 * n_samples) + np.exp(
-#         monitor.log_alphas[-1]) * norm(clf2.coef_, ord=1))
+p_alphas_grad = np.exp(np.array(monitor.log_alphas)) / alpha_max
+objs_grad = np.array(monitor.objs)
+
+current_palette = sns.color_palette("colorblind")
+
+
+fig = plt.figure()
+plt.semilogx(
+    p_alphas, objs, color=current_palette[0], linewidth=7.0)
+plt.semilogx(
+    p_alphas, objs, 'bo', label='0-order method (grid-search)',
+    color=current_palette[1], markersize=15)
+plt.semilogx(
+    p_alphas_grad, objs_grad, 'bX', label='1-st order method',
+    color=current_palette[2], markersize=25)
+plt.xlabel(r"$\lambda / \lambda_{\max}$", fontsize=28)
+plt.ylabel(
+    "Cross-validation loss",
+    fontsize=28)
+axes = plt.gca()
+# axes.set_ylim([0, 1])
+plt.tick_params(width=5)
+plt.legend(fontsize=14, loc=1)
+plt.tight_layout()
+plt.show(block=False)
