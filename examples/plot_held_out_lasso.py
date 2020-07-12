e3817ef0166aacf20248f2b9dcb45393e2714bb9
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2020-07-13 00:14:58 +0200

    nicer examples (#7)
    * nicer examples
    
    * iter
    
    * missing
    
    * more readable figures
    
    * more
    
    * retrigger_ci
    
    * use simu to make circleci happy
    
    * retrigger_ci

--- examples/plot_held_out_lasso.py
+++ examples/plot_held_out_lasso.py
@@ -1,8 +1,10 @@
 """
-=======================================================================
-Example Lasso and the held-out loss
-=======================================================================
-...
+============================
+Lasso with held-out test set
+============================
+
+This example shows how to perform hyperparameter optimisation
+for a Lasso using a held-out test set.
 
 """
 
@@ -22,12 +24,20 @@ from sparse_ho.implicit_forward import ImplicitForward
 from sparse_ho.utils import Monitor
 from sparse_ho.ho import grad_search
 from sparse_ho.grid_search import grid_search
-# from sparse_ho.datasets.real import get_real_sim
+from sparse_ho.datasets.real import get_real_sim
 from sparse_ho.datasets.real import get_rcv1
 
+print(__doc__)
 
-X_train, X_val, X_test, y_train, y_val, y_test = get_rcv1()
-# X_train, X_val, X_test, y_train, y_val, y_test = get_real_sim()
+# dataset = 'rcv1'
+dataset = 'simu'
+
+if dataset == 'rcv1':
+    X_train, X_val, X_test, y_train, y_val, y_test = get_rcv1()
+else:
+    X_train, X_val, X_test, y_train, y_val, y_test = get_real_sim()
+
+
 n_samples, n_features = X_train.shape
 
 print("Starting path computation...")
@@ -42,8 +52,10 @@ log_alphas = np.log(alphas)
 
 tol = 1e-7
 
-# grid search
-model = Lasso(X_train, y_train, np.log(alpha_max/10))
+##############################################################################
+# Grid-search
+# -----------
+model = Lasso(X_train, y_train, np.log(alpha_max / 10))
 criterion = CV(X_val, y_val, model, X_test=X_test, y_test=y_test)
 algo = Forward(criterion, use_sk=True)
 monitor_grid_sk = Monitor()
@@ -52,35 +64,39 @@ grid_search(
     tol=tol)
 objs = np.array(monitor_grid_sk.objs)
 
-# grad_search
-model = Lasso(X_train, y_train, np.log(alpha_max/10))
+##############################################################################
+# Grad-search
+# -----------
+model = Lasso(X_train, y_train, np.log(alpha_max / 10))
 criterion = CV(X_val, y_val, model, X_test=X_test, y_test=y_test)
 algo = ImplicitForward(criterion, use_sk=True)
 monitor_grad = Monitor()
 grad_search(
-    algo, np.log(alpha_max/10), monitor_grad, n_outer=10, tol=tol)
+    algo, np.log(alpha_max / 10), monitor_grad, n_outer=10, tol=tol)
 
 
+##############################################################################
+# Plot results
+# ------------
 p_alphas_grad = np.exp(np.array(monitor_grad.log_alphas)) / alpha_max
 
 objs_grad = np.array(monitor_grad.objs)
 
 current_palette = sns.color_palette("colorblind")
 
-fig = plt.figure()
+fig = plt.figure(figsize=(5, 3))
 plt.semilogx(
-    p_alphas, objs, color=current_palette[0], linewidth=7.0)
+    p_alphas, objs, color=current_palette[0])
 plt.semilogx(
     p_alphas, objs, 'bo', label='0-order method (grid-search)',
-    color=current_palette[1], markersize=15)
+    color=current_palette[1])
 plt.semilogx(
     p_alphas_grad, objs_grad, 'bX', label='1-st order method',
-    color=current_palette[2], markersize=25)
-plt.xlabel(r"$\lambda / \lambda_{\max}$", fontsize=28)
+    color=current_palette[2])
+plt.xlabel(r"$\lambda / \lambda_{\max}$")
 plt.ylabel(
-    r"$\|y^{\rm{val}} - X^{\rm{val}} \hat \beta^{(\lambda)} \|^2$",
-    fontsize=28)
+    r"$\|y^{\rm{val}} - X^{\rm{val}} \hat \beta^{(\lambda)} \|^2$")
 plt.tick_params(width=5)
-plt.legend(fontsize=14, loc=1)
+plt.legend()
 plt.tight_layout()
 plt.show(block=False)
