ea80388cc166b513cb887ae02dbdd095a843efa4
QB3 <quentin.bertrand@inria.fr>
2020-08-04 18:23:24 +0200

    modifed examples

--- examples/plot_held_out_enet.py
+++ examples/plot_held_out_enet.py
@@ -13,6 +13,7 @@ for an elastic-net using a held-out validation set.
 #
 # License: BSD (3-clause)
 
+import time
 from sparse_ho.datasets.real import get_data
 import numpy as np
 from sklearn.linear_model import ElasticNet as ElasticNet_sk
@@ -24,6 +25,8 @@ from sparse_ho.criterion import CV
 from sparse_ho.models import ElasticNet
 from sparse_ho.ho import grad_search
 from sparse_ho.utils import Monitor
+
+Axes3D  # hack for weird matplotlib bug
 
 dataset = "rcv1"
 # use_small_part = False
@@ -47,27 +50,36 @@ log_alpha_max = np.log(alpha_max)
 
 alpha_min = 1e-4 * alpha_max
 
-n_grid = 10
-alphas_1 = np.geomspace(alpha_min, 0.6 * alpha_max, n_grid)
+n_grid = 20
+alphas_1 = np.geomspace(0.6 * alpha_max, alpha_min, n_grid)
 log_alphas_1 = np.log(alphas_1)
-alphas_2 = np.geomspace(alpha_min, 0.6 * alpha_max, n_grid)
+alphas_2 = np.geomspace(0.6 * alpha_max, alpha_min, n_grid)
 log_alphas_2 = np.log(alphas_2)
 
 results = np.zeros((n_grid, n_grid))
-tol = 1e-5
+tol = 1e-7
 max_iter = 50000
 
 # grid search with scikit
+print("Started grid-search")
+t_grid_search = - time.time()
 for i in range(n_grid):
+    print("lambda %i / %i" % (i, n_grid))
     for j in range(n_grid):
+        print("lambda %i / %i" % (j, n_grid))
         clf = ElasticNet_sk(
             alpha=(alphas_1[i] + alphas_2[j]), fit_intercept=False,
             l1_ratio=alphas_1[i] / (alphas_1[i] + alphas_2[j]),
-            tol=1e-7, max_iter=max_iter, warm_start=True)
+            tol=tol, max_iter=max_iter, warm_start=True)
         clf.fit(X_train, y_train)
         results[i, j] = norm(y_val - X_val @ clf.coef_) ** 2 / X_val.shape[0]
+t_grid_search += time.time()
+print("Finished grid-search")
+
 
 # grad search
+print("Started grad-search")
+t_grad_search = - time.time()
 monitor = Monitor()
 n_outer = 20
 model = ElasticNet(
@@ -75,16 +87,19 @@ model = ElasticNet(
 criterion = CV(
     X_val, y_val, model, X_test=X_test, y_test=y_test)
 algo = ImplicitForward(
-    criterion, tol_jac=1e-5, n_iter_jac=1000, use_sk=True,
+    criterion, tol_jac=1e-3, n_iter_jac=100, use_sk=True,
     max_iter=max_iter)
 _, _, _ = grad_search(
-    algo=algo, verbose=False,
+    algo=algo, verbose=True,
     log_alpha0=np.array([log_alphas_1[8], log_alphas_2[8]]), tol=tol,
     n_outer=n_outer, monitor=monitor,
-    tolerance_decrease='constant')
+    tolerance_decrease='exponential')
+t_grad_search += time.time()
 alphas_grad = np.exp(np.array(monitor.log_alphas))
 alphas_grad /= alpha_max
 
+
+# plot results
 
 X, Y = np.meshgrid(alphas_1 / alpha_max, alphas_2 / alpha_max)
 fig = plt.figure()
@@ -98,5 +113,8 @@ ax.scatter3D(
 ax.set_xlabel("lambda1")
 ax.set_ylabel("lambda2")
 ax.set_label("Loss on validation set")
-Axes3D(fig)
 fig.show()
+
+
+print("Time grid-search %f" % t_grid_search)
+print("Time grad-search %f" % t_grad_search)
