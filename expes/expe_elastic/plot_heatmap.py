0d90596f2d5e34722aaae25d4f1f7e8f6878a6fa
QB3 <quentin.bertrand@inria.fr>
2020-09-16 14:50:28 +0200

    TEX modified plottting files

--- expes/expe_elastic/plot_heatmap.py
+++ expes/expe_elastic/plot_heatmap.py
@@ -2,8 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sparse_ho.utils_plot import configure_plt
 
-save_fig = True
-# save_fig = False
+# save_fig = True
+save_fig = False
+save_fig_grad = True
+# save_fig_grad = False
 fig_dir = "../../../CD_SUGAR/tex/slides_qbe_long/prebuiltimages/"
 fig_dir_svg = "../../../CD_SUGAR/tex/slides_qbe_long/images/"
 
@@ -26,62 +28,89 @@ levels = np.round(levels, 2)
 
 fontsize = 15
 
-# plt.set_cmap(plt.cm.viridis)
+plt.set_cmap(plt.cm.viridis)
+
 # fig, ax = plt.subplots(1, 1)
-# cp = ax.contourf(np.log10(X), np.log10(Y), Z.T, levels)
+# cp = ax.contourf(X, Y, Z.T, levels)
 # ax.scatter(
-#     X, Y, s=10, c="yellow", marker="o")
+#     X, Y, s=10, c="orange", marker="o", label="$0$ order (grid search)")
+# # cp.ax.tick_params(labelsize=2)
 # ax.scatter(
-#     # alphas_grad[:, 0]/alpha_max,
-#     # alphas_grad[:, 1]/alpha_max,
-#     np.log10(alphas_grad[:, 0]/alpha_max),
-#     np.log10(alphas_grad[:, 1]/alpha_max),
-#     s=100, color=[plt.cm.Reds((i + 1) / len(objs_grad)) for i in np.arange(
-#         len(objs_grad))], marker="x")
-# fig.colorbar(cp)
-# # plt.xscale('log')
-# # plt.yscale('log')
-# plt.title(r'Real-sim dataset, elastic net: $\min_\beta \frac{1}{2n} ||y-X\beta||^2 + \lambda_1||\beta||_1 + \frac{\lambda_2}{2}||\beta||_2^2$')
-# # fig.legend()
-# ax.set_xlabel(r'$\lambda_1 / \lambda_\max$')
-# ax.set_ylabel(r'$\lambda_2/ \lambda_\max$')
+#     alphas_grad[:, 0]/alpha_max,
+#     alphas_grad[:, 1]/alpha_max,
+#     s=100, color=[plt.cm.Reds((i + len(objs_grad) / 5 + 1) / len(objs_grad)) for i in np.arange(
+#         len(objs_grad))], marker="x", label="$1$st order")
+# cb = fig.colorbar(cp)
+# for t in cb.ax.get_yticklabels():
+#     t.set_fontsize(fontsize)
+# ax.set_xlim(alpha_1.min()/alpha_max, alpha_1.max()/alpha_max)
+# ax.set_ylim(alpha_2.min()/alpha_max, alpha_2.max()/alpha_max)
+# plt.xscale('log')
+# plt.yscale('log')
+# fig.legend(loc=2, ncol=2, fontsize=fontsize, bbox_to_anchor=(0.1653, 1))
+# ax.set_xlabel(r'$\lambda_1 / \lambda_\max$', fontsize=fontsize)
+# ax.set_ylabel(r'$\lambda_2 / \lambda_\max$', fontsize=fontsize)
+# # ax.set_xticklabels(fontsize=fontsize)
+# plt.xticks(fontsize=fontsize)
+# plt.yticks(fontsize=fontsize)
+# # plt.tight_layout()
+
+# if save_fig:
+#     fig.savefig(
+#         fig_dir + "held_out_real_sim_enet.pdf", bbox_inches="tight")
+#     fig.savefig(
+#         fig_dir_svg + "held_out_real_sim_enet.svg", bbox_inches="tight")
+
 # plt.show(block=False)
 
 
-plt.set_cmap(plt.cm.viridis)
-fig, ax = plt.subplots(1, 1)
-cp = ax.contourf(X, Y, Z.T, levels)
-ax.scatter(
-    X, Y, s=10, c="orange", marker="o", label="$0$ order (grid search)")
-# cp.ax.tick_params(labelsize=2)
-ax.scatter(
-    alphas_grad[:, 0]/alpha_max,
-    alphas_grad[:, 1]/alpha_max,
-    s=100, color=[plt.cm.Reds((i + len(objs_grad) / 5 + 1) / len(objs_grad)) for i in np.arange(
-        len(objs_grad))], marker="x", label="$1$st order")
-# ax.scatter(
-#     alphas_grad[-1, 0]/alpha_max,
-#     alphas_grad[-1, 1]/alpha_max,
-#     s=100, color=[plt.cm.Reds(1.0)], marker="x", label="$1$st order")
-cb = fig.colorbar(cp)
-for t in cb.ax.get_yticklabels():
-    t.set_fontsize(fontsize)
-ax.set_xlim(alpha_1.min()/alpha_max, alpha_1.max()/alpha_max)
-ax.set_ylim(alpha_2.min()/alpha_max, alpha_2.max()/alpha_max)
-plt.xscale('log')
-plt.yscale('log')
-fig.legend(loc=2, ncol=2, fontsize=fontsize, bbox_to_anchor=(0.1653, 1))
-ax.set_xlabel(r'$\lambda_1 / \lambda_\max$', fontsize=fontsize)
-ax.set_ylabel(r'$\lambda_2 / \lambda_\max$', fontsize=fontsize)
-# ax.set_xticklabels(fontsize=fontsize)
-plt.xticks(fontsize=fontsize)
-plt.yticks(fontsize=fontsize)
-# plt.tight_layout()
+color = [
+    plt.cm.Reds((i + len(objs_grad) / 5 + 1) / len(objs_grad))
+    for i in np.arange(len(objs_grad))]
+#################################################################
+# code for GRID SEARCH + GRAD SEARCH
+for i in np.arange(len(objs_grad)+1):
+    fig, ax = plt.subplots(1, 1)
+    cp = ax.contourf(X, Y, Z.T, levels)
+    ax.scatter(
+        X, Y, s=10, c="orange", marker="o", label="$0$ order (grid search)")
+    cb = fig.colorbar(cp)
+    for t in cb.ax.get_yticklabels():
+        t.set_fontsize(fontsize)
+    # ax.plot(
+    #     p_alphas, objs, color=current_palette[0], linewidth=7.0)
+    # ax.plot(
+    #     p_alphas, objs, 'bo', label='0-order method',
+    #     color=current_palette[1], markersize=15)
+    ax.scatter(
+        alphas_grad[:i, 0]/alpha_max,
+        alphas_grad[:i, 1]/alpha_max,
+        s=100, color=color[:i], marker="x", label="$1$st order")
+    plt.xscale('log')
+    plt.yscale('log')
+    ax.set_xlim(alpha_1.min()/alpha_max, alpha_1.max()/alpha_max)
+    ax.set_ylim(alpha_2.min()/alpha_max, alpha_2.max()/alpha_max)
+    plt.xscale('log')
+    plt.yscale('log')
+    fig.legend(loc=2, ncol=2, fontsize=fontsize, bbox_to_anchor=(0.125, 1))
+    ax.set_xlabel(r'$\lambda_1 / \lambda_\max$', fontsize=fontsize)
+    ax.set_ylabel(r'$\lambda_2 / \lambda_\max$', fontsize=fontsize)
+    # ax.set_xticklabels(fontsize=fontsize)
+    plt.xticks(fontsize=fontsize)
+    plt.yticks(fontsize=fontsize)
+    # ax.set_xlim(p_alphas.min(), p_alphas.max())
+    # ax.set_ylim(ymin, ymax)
+    # plt.xlabel(r"$\lambda / \lambda_{\max}$", fontsize=28)
+    # plt.ylabel(
+    #     r"$\|y^{\rm{val}} - X^{\rm{val}} \hat \beta^{(\lambda)} \|^2$",
+    #     fontsize=28)
+    # plt.tick_params(width=5)
+    # plt.legend(fontsize=17, loc=2)
+    # plt.tight_layout()
 
-if save_fig:
-    fig.savefig(
-        fig_dir + "held_out_real_sim_enet.pdf", bbox_inches="tight")
-    fig.savefig(
-        fig_dir_svg + "held_out_real_sim_enet.svg", bbox_inches="tight")
-
-plt.show(block=False)
+    if save_fig_grad:
+        fig.savefig(
+            fig_dir + "grad_grid_search_real_sim_enet_%i.pdf" % i, bbox_inches="tight")
+        fig.savefig(
+            fig_dir_svg + "grad_grid_search_real_sim_enet_%i.svg" % i, bbox_inches="tight")
+    plt.show(block=False)
