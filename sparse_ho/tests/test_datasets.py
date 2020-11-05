0c60d56f54dcb60ffd21e1f0163f1a796751e262
QB3 <quentin.bertrand@inria.fr>
2020-11-05 10:53:17 +0100

    fix test dataset

--- sparse_ho/tests/test_datasets.py
+++ sparse_ho/tests/test_datasets.py
@@ -4,7 +4,8 @@ from sparse_ho.datasets.real import get_data
 
 
 def test_20news():
-    X_train, X_val, X_test, y_train, y_val, y_test = get_data("20news")
+    X_train, X_val, X_test, y_train, y_val, y_test = get_data("rcv1_train")
+    # X_train, X_val, X_test, y_train, y_val, y_test = get_data("news20")
 
     np.testing.assert_equal(X_train.shape[0], y_train.shape[0])
     np.testing.assert_equal(X_test.shape[0], y_test.shape[0])
