9e0af2fee1cb298951dd10933ddea3a0c1b4c61e
QB3 <quentin.bertrand@inria.fr>
2020-11-06 16:51:26 +0100

    ENH improved wLasso example

--- examples/plot_wlasso.py
+++ examples/plot_wlasso.py
@@ -10,90 +10,93 @@ for a weighted Lasso using a held-out validation set.
 
 # Authors: Quentin Bertrand <quentin.bertrand@inria.fr>
 #          Quentin Klopfenstein <quentin.klopfenstein@u-bourgogne.fr>
-#
+#          Mathurin Massias
+#          Kenan Sehic
 # License: BSD (3-clause)
-
-import time
 
 import numpy as np
 
-from sklearn.datasets import make_regression
-from sklearn.model_selection import train_test_split
+from celer import Lasso, LassoCV
 
-from celer import Lasso
+from sklearn.datasets import make_regression
 
 from sparse_ho.models import wLasso
 from sparse_ho.criterion import CV
 from sparse_ho.implicit_forward import ImplicitForward
 from sparse_ho.utils import Monitor
 from sparse_ho.ho import grad_search
-from sparse_ho.datasets import get_data
 
-print(__doc__)
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
 
-dataset = 'rcv1_train'
-# dataset = 'simu'
+# from wlasso_hpo import sparse_ho_model, random_search
 
-if dataset != 'simu':
-    X_train, X_val, X_test, y_train, y_val, y_test = get_data(dataset)
-    X_train = X_train[:, :1000]
-    X_test = X_test[:, :1000]
-else:
-    X, y = make_regression(n_samples=100, n_features=100, noise=1)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
-    X_train, X_val, y_train, y_val = train_test_split(
-        X_train, y_train, test_size=0.5)
 
-n_samples, n_features = X_train.shape
+X, y = make_regression(n_samples=500, n_features=600, noise=40, random_state=2)
+X_train_val, X_test, y_train_val, y_test = train_test_split(
+    X, y, test_size=0.15)
 
-print("Starting path computation...")
-n_samples = len(y_train)
+all_indices = list(range(X_train_val.shape[0]))
+train_indices, val_indices = train_test_split(
+    all_indices, test_size=0.5, random_state=42)
+
+X_train = X_train_val[train_indices, :]
+X_val = X_train_val[val_indices, :]
+
+y_train = y_train_val[train_indices]
+y_val = y_train_val[val_indices]
+
+
+# max penalty value
 alpha_max = np.max(np.abs(X_train.T.dot(y_train))) / X_train.shape[0]
-log_alpha0 = np.log(alpha_max / 10)
 
-n_alphas = 10
-p_alphas = np.geomspace(1, 0.0001, n_alphas)
-alphas = alpha_max * p_alphas
-log_alphas = np.log(alphas)
+n_alphas = 30
+alphas = alpha_max * np.geomspace(1, 0.001, n_alphas)
 
-tol = 1e-7
-max_iter = 1e5
-
-##############################################################################
-# Grid-search
-# -----------
-
-# The sklearn solver is indeed very long on the considered problems!
-# estimator = linear_model.Lasso(
-#     fit_intercept=False, max_iter=1000, warm_start=True)
-
-# celer is way faster !
-# https://github.com/mathurinm/celer
-
-estimator = Lasso(
-    fit_intercept=False, max_iter=1000, warm_start=True)
+train_split_cv = [train_indices]
+val_split_cv = [val_indices]
 
 
-##############################################################################
-# Grad-search
-# -----------
-print('sparse-ho started')
+# vanilla LassoCV
+print("========== Celer's LassoCV started ===============")
+model_cv = LassoCV(verbose=False, fit_intercept=False,
+                   alphas=alphas, tol=1e-7, max_iter=100,
+                   cv=zip(train_split_cv, val_split_cv)).fit(X_train_val,
+                                                             y_train_val)
 
-alpha0 = np.log(alpha_max / 10) * np.ones(n_features)
 
-t0 = time.time()
-model = wLasso(X_train, y_train, estimator=estimator)
+# measure mse on test
+mse_cv = mean_squared_error(y_test,  model_cv.predict(X_test))
+print("========== Celer's LassoCV finished ===============")
+print("Vanilla LassoCV: Mean-squared error on test data %f" % mse_cv)
 
-# Here CV means held out,
-# the "real" crossval (with folds, etc.) is very slow (for the moment) for some
-# unknown reasons
 
-criterion = CV(X_val, y_val, model, X_test=X_test, y_test=y_test)
-algo = ImplicitForward(criterion)
-monitor_grad = Monitor()
+# let's run sparse-ho with vanilla lassoCV initial config
+alpha0 = np.log(model_cv.alpha_) * np.ones(X_train.shape[1])
+
+###
+# Sparse-ho
+lasso_sho = Lasso(fit_intercept=False, max_iter=1000, warm_start=True)
+model_sho = wLasso(X_train, y_train, estimator=lasso_sho)
+criterion_sho = CV(X_val, y_val, model_sho, X_test=X_test, y_test=y_test)
+algo_sho = ImplicitForward(criterion_sho)
+monitor = Monitor()
 grad_search(
-    algo, alpha0, monitor_grad, n_outer=10, tol=tol)
+    algo_sho, alpha0, monitor, n_outer=20, tol=1e-6)
 
-t_grad_search = time.time() - t0
 
-print("Time gradient serach:  %f" % t_grad_search)
+alphas = np.exp(monitor.log_alphas[-1])
+# X_test /= alphas
+lasso_sho.fit(X_train / alphas, y_train)
+coef_ = lasso_sho.coef_ / alphas
+
+
+# mse on validation set
+mse_sho_val = mean_squared_error(y_val, X_val @ coef_)
+
+# mse on test set, ie unseen data
+mse_sho_test = mean_squared_error(y_test, X_test @ coef_)
+
+print("========== Sparse-ho finished ===============")
+print("Sparse-ho: Mean-squared error on validation data %f" % mse_sho_val)
+print("Sparse-ho: Mean-squared error on test (unseen) data %f" % mse_sho_test)
