b2d102e3514bdd2277eb1b1698664e35538a652a
QB3 <quentin.bertrand@inria.fr>
2020-11-11 11:29:13 +0100

    ENH added explicit noise level + change simulation method

--- examples/plot_wlasso.py
+++ examples/plot_wlasso.py
@@ -16,11 +16,13 @@ In particular we compare the weighted Lasso to LassoCV on a toy example
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from numpy.linalg import norm
 
 from celer import Lasso, LassoCV
-from sklearn.datasets import make_regression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
+from sklearn.utils import check_random_state
+from scipy.linalg import toeplitz
 
 from sparse_ho.models import wLasso
 from sparse_ho.criterion import CV
@@ -31,16 +33,33 @@ from sparse_ho.ho import grad_search
 ##############################################################################
 # Dataset creation
 ##############################################################################
-X, y = make_regression(n_samples=600, n_features=600, noise=60, random_state=2)
+# X, y = make_regression(n_samples=600, n_features=600, noise=60, random_state=2)
+n_samples = 600
+n_features = 600
+rng = check_random_state(0)
+X = rng.multivariate_normal(
+    size=n_samples, mean=np.zeros(n_features),
+    cov=toeplitz(0.5 ** np.arange(n_features)))
+
+
+# Create true regression coefficients of 5 non-zero values
+w_true = np.zeros(n_features)
+size_supp = 5
+idx = rng.choice(
+    X.shape[0], size_supp, replace=False)
+w_true[idx] = (-1) ** np.arange(size_supp)
+noise = rng.randn(n_samples)
+y = X @ w_true
+y += noise / norm(noise) * 0.5 * norm(y)
 
 # Here we split the dataset (X, y) in 3:
 # the regression coefficients will be determined using X_train, y_train
 # the regularization parameter will be calibrated using X_val, y_val
 # the model is then tested on unseen data X_test, y_test
 X_train_val, X_test, y_train_val, y_test = train_test_split(
-    X, y, test_size=0.3)
+    X, y, test_size=0.3, random_state=2)
 X_train, X_val, y_train, y_val = train_test_split(
-    X_train_val, y_train_val, test_size=0.5)
+    X_train_val, y_train_val, test_size=0.5, random_state=2)
 ##############################################################################
 
 
@@ -80,11 +99,6 @@ grad_search(
     algo_sho, alpha0, monitor, n_outer=20, tol=1e-6)
 
 
-# alphas = np.exp(monitor.log_alphas[-1])
-# lasso_sho.fit(X_train, y_train)
-# coef_ = lasso_sho.coef_ / alphas
-
-
 # mse on validation set
 mse_sho_val = mean_squared_error(y_val, lasso_sho.predict(X_val))
 
@@ -96,20 +110,11 @@ print("Sparse-ho: Mean-squared error on validation data %f" % mse_sho_val)
 print("Sparse-ho: Mean-squared error on test (unseen) data %f" % mse_sho_test)
 
 
-# fig = plt.figure()
-# ax = fig.add_axes([0, 0, 1, 1])
-# legend = ['wLasso val', 'wLasso test', 'Lasso CV']
-# mses = [mse_sho_val, mse_sho_test, mse_cv]
-# ax.bar(legend, mses)
-# ax.legend(labels=legend)
-# plt.show(block=False)
-
 labels = ['wLasso val', 'wLasso test', 'Lasso CV']
 
 df = pd.DataFrame(
     np.array([mse_sho_val, mse_sho_test, mse_cv]).reshape((1, -1)),
     columns=labels)
-# df.index = labels
 df.plot.bar(rot=0)
 plt.xlabel("Estimator")
 plt.ylabel("Mean square error")
