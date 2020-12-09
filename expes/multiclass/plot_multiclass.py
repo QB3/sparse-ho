7cf0bd5a225db90fe8252157437ef6909da0ca54
QB3 <quentin.bertrand@inria.fr>
2020-12-09 10:27:17 +0100

    [ci skip] added back expes

--- expes/multiclass/plot_multiclass.py
+++ expes/multiclass/plot_multiclass.py
@@ -17,24 +17,25 @@ from sparse_ho.ho import grad_search
 from sparse_ho.utils import Monitor
 from sparse_ho.datasets.utils_datasets import (
     get_alpha_max, clean_dataset, get_splits)
+from sparse_ho.hyperopt_wrapper import hyperopt_wrapper
 
 # load data
-n_samples = 100
-n_features = 100
+n_samples = 1_000
+n_features = 1_000
 # n_samples = 1_100
 # n_features = 3_200
 # X, y = fetch_libsvm('sensit')
 # X, y = fetch_libsvm('usps')
-# X, y = fetch_libsvm('rcv1_multiclass')
+X, y = fetch_libsvm('rcv1_multiclass')
 # X, y = fetch_libsvm('sector_scale')
 # X, y = fetch_libsvm('sector')
 # X, y = fetch_libsvm('smallNORB')
-X, y = fetch_libsvm('mnist')
+# X, y = fetch_libsvm('mnist')
 
 
 # clean data and subsample
 X, y = clean_dataset(X, y, n_samples, n_features)
-idx_train, idx_val, _ = get_splits(X, y)
+idx_train, idx_val, idx_test = get_splits(X, y)
 n_samples, n_features = X.shape
 
 algo = ImplicitForward(n_iter_jac=1000)
@@ -42,7 +43,7 @@ estimator = LogisticRegression(
     C=1, fit_intercept=False, warm_start=True, max_iter=2000, verbose=False)
 
 model = SparseLogreg(estimator=estimator)
-logit_multiclass = LogisticMulticlass(idx_train, idx_val, algo)
+logit_multiclass = LogisticMulticlass(idx_train, idx_val, idx_test, algo)
 
 
 alpha_max, n_classes = get_alpha_max(X, y)
@@ -62,15 +63,25 @@ for i in range(n_alphas):
 
 1/0
 print("###################### GRAD SEARCH LS ###################")
-n_outer = 1000
+n_outer = 100
 model = SparseLogreg(estimator=estimator)
-logit_multiclass = LogisticMulticlass(idx_train, idx_val, algo)
+logit_multiclass = LogisticMulticlass(idx_train, idx_val, idx_test, algo)
 
 monitor = Monitor()
 log_alpha0 = np.ones(n_classes) * np.log(0.1 * alpha_max)
 
 idx_min = np.argmin(np.array(monitor_grid.objs))
 log_alpha0 = monitor_grid.log_alphas[idx_min]
-optimizer = GradientDescent(n_outer=n_outer, step_size=0.00001, tol=tol)
+optimizer = GradientDescent(
+    n_outer=n_outer, step_size=None, p_grad0=0.1, tol=tol)
 grad_search(
     algo, logit_multiclass, model, optimizer, X, y, log_alpha0, monitor)
+
+
+print("###################### USE HYPEROPT ###################")
+log_alpha_max = np.log(alpha_max)
+log_alpha_min = np.log(alpha_max / 10_000)
+monitor_hyp = Monitor()
+hyperopt_wrapper(
+    algo, logit_multiclass, model, X, y, log_alpha_min, log_alpha_max,
+    monitor_hyp, tol=tol, size_space=n_classes, max_evals=10)
