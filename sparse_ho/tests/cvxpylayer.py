a1ad0a338c326f86b8d1775e0c054a83b0ee3e7a
QB3 <quentin.bertrand@inria.fr>
2021-01-14 18:09:03 +0100

    WIP added sure

--- sparse_ho/tests/cvxpylayer.py
+++ sparse_ho/tests/cvxpylayer.py
@@ -2,6 +2,7 @@ import cvxpy as cp
 import numpy as np
 import torch
 from cvxpylayers.torch import CvxpyLayer
+from sklearn.utils import check_random_state
 
 torch.set_default_dtype(torch.double)
 
@@ -123,3 +124,114 @@ def logreg_cvxpy(X, y, alpha, idx_train, idx_val):
     val = test_loss.detach().numpy()
     grad = np.array(alpha_th.grad)
     return val, grad
+
+
+def lasso_sure_cvxpy(X, y, alpha, sigma, random_state=42):
+    # lambda_alpha = [alpha, alpha]
+    n_samples, n_features = X.shape
+    epsilon = 2 * sigma / n_samples ** 0.3
+    rng = check_random_state(random_state)
+    delta = rng.randn(n_samples)
+
+    y2 = y + epsilon * delta
+    Xth, yth, y2th, deltath = map(torch.from_numpy, [X, y, y2, delta])
+
+    n_samples_train, n_features = Xth.shape
+
+    # set up variables and parameters
+    beta_cp = cp.Variable(n_features)
+    lambda_cp = cp.Parameter(nonneg=True)
+
+    # set up objective
+    loss = ((1 / (2 * n_samples_train)) * cp.sum(
+        cp.square(Xth @ beta_cp - yth)))
+    reg = lambda_cp * cp.norm1(beta_cp)
+    objective = loss + reg
+
+    # define problem
+    problem1 = cp.Problem(cp.Minimize(objective))
+    assert problem1.is_dpp()
+
+    # solve problem1
+    layer = CvxpyLayer(problem1, [lambda_cp], [beta_cp])
+    alpha_th1 = torch.tensor(alpha, requires_grad=True)
+    beta1, = layer(alpha_th1)
+
+    # get test loss and it's gradient
+    test_loss1 = (Xth @ beta1 - yth).pow(2).sum()
+    test_loss1 -= 2 * sigma ** 2 / epsilon * (Xth @ beta1) @ deltath
+    test_loss1.backward()
+    val1 = test_loss1.detach().numpy()
+    grad1 = np.array(alpha_th1.grad)
+
+    # set up variables and parameters
+    beta_cp = cp.Variable(n_features)
+    lambda_cp = cp.Parameter(nonneg=True)
+
+    # set up objective
+    loss = ((1 / (2 * n_samples_train)) * cp.sum(
+        cp.square(Xth @ beta_cp - y2th)))
+    reg = lambda_cp * cp.norm1(beta_cp)
+    objective = loss + reg
+
+    # define problem
+    problem2 = cp.Problem(cp.Minimize(objective))
+    assert problem2.is_dpp()
+
+    # solve problem2
+    layer = CvxpyLayer(problem2, [lambda_cp], [beta_cp])
+    alpha_th2 = torch.tensor(alpha, requires_grad=True)
+    beta2, = layer(alpha_th2)
+
+    # get test loss and it's gradient
+    test_loss1 = 2 * sigma ** 2 / epsilon * (Xth @ beta2) @ deltath
+    test_loss1.backward()
+    val2 = test_loss1.detach().numpy()
+    grad2 = np.array(alpha_th2.grad)
+
+    val = val1 + val2
+    grad = grad1 + grad2
+    return val, grad
+
+# import pytest
+# import itertools
+
+# import numpy as np
+# from scipy.optimize import check_grad
+# from scipy.sparse import csc_matrix
+# from sklearn import linear_model
+# import celer
+
+# from sparse_ho.datasets.synthetic import get_synt_data
+# from sparse_ho.models import Lasso, ElasticNet, WeightedLasso, SparseLogreg
+
+# from sparse_ho import Forward, ImplicitForward, Implicit
+
+# from sparse_ho.algo.forward import get_beta_jac_iterdiff
+# from sparse_ho.algo.implicit_forward import get_beta_jac_fast_iterdiff
+# from sparse_ho.algo.implicit import get_beta_jac_t_v_implicit
+# from sparse_ho.criterion import (
+#     HeldOutMSE, FiniteDiffMonteCarloSure, HeldOutLogistic)
+# from sparse_ho.tests.cvxpylayer import \
+#     enet_cvxpy, weighted_lasso_cvxpy, logreg_cvxpy, lasso_cvxpy
+
+
+# # Generate data
+# n_samples, n_features = 10, 10
+# X, y, _, _, sigma_star = get_synt_data(
+#     dictionary_type="Toeplitz", n_samples=n_samples,
+#     n_features=n_features, n_times=1, n_active=5, rho=0.1,
+#     SNR=3, seed=0)
+# y = np.sign(y)
+# idx_train = np.arange(0, n_features//2)
+# idx_val = np.arange(n_features//2, n_features)
+
+# # Set alpha for the Lasso
+# alpha_max = (np.abs(X[idx_train, :].T @ y[idx_train])).max() / n_samples
+# p_alpha = 0.8
+# alpha = p_alpha * alpha_max
+# sigma = 1
+
+# val, grad = lasso_sure_cvxpy(X, y, alpha, sigma)
+
+# # lasso_cvxpy(X, y, alpha, idx_train, idx_val)
