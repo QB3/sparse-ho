bf5854e196fdaf2cee87c740c163db192ea8d4b9
QB3 <quentin.bertrand@inria.fr>
2021-01-15 14:58:27 +0100

    new try to install celer

--- setup.py
+++ setup.py
@@ -49,9 +49,11 @@ if __name__ == "__main__":
           ],
           platforms='any',
           packages=find_packages(),
-          install_requires=["celer>=0.6", "download", "hyperopt",
-                            "libsvmdata", "matplotlib>=2.0.0", "numba",
-                            "numpy>=1.12", "scipy>=0.18.0",
-                            "scikit-learn>=0.21", "seaborn>=0.7", ],
-          dependency_links=['https://github.com/mathurinm/celer']
+          install_requires=[
+            "celer @ git+ssh://www.github.com/mathurinm/celer/",
+            "download", "hyperopt",
+            "libsvmdata", "matplotlib>=2.0.0", "numba",
+            "numpy>=1.12", "scipy>=0.18.0",
+            "scikit-learn>=0.21", "seaborn>=0.7", ]
+        #   dependency_links=['https://github.com/mathurinm/celer']
           )
