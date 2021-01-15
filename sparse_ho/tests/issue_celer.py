f9ddb7d8cf89a4a9eab70c0986f4098e9846bda9
QB3 <quentin.bertrand@inria.fr>
2021-01-15 16:54:13 +0100

    rm useless file

--- sparse_ho/tests/issue_celer.py
+++
@@ -1,7 +0,0 @@
-from celer.datasets.simulated import make_correlated_data
-
-y = make_correlated_data(10, 10, random_state=42)[1]
-print("y.sum() = %5f " % y.sum())
-
-y = make_correlated_data(10, 10, random_state=42)[1]
-print("y.sum() = %5f " % y.sum())
