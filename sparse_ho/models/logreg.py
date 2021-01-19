baeb6d1d0bf0dc252ec605d63cf94b70f041a87a
Quentin Bertrand <quentin.bertrand@inria.fr>
2021-01-19 15:22:47 +0100

    Update sparse_ho/models/logreg.py
    Co-authored-by: Alexandre Gramfort <alexandre.gramfort@m4x.org>

--- sparse_ho/models/logreg.py
+++ sparse_ho/models/logreg.py
@@ -130,7 +130,7 @@ class SparseLogreg(BaseModel):
 
     @staticmethod
     def _get_pobj(r, X, beta, alphas, y):
-        pobj = np.log(1 + np.exp(- r)).mean() + np.abs(alphas * beta).sum()
+        pobj = np.log1p(np.exp(- r)).mean() + np.abs(alphas * beta).sum()
         return pobj
 
     @staticmethod
