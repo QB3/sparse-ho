333803b62ec492ca3d483169064beffd4a7129ae
QB3 <quentin.bertrand@inria.fr>
2021-01-20 11:34:43 +0100

    make test to see get_val fail for logreg

--- sparse_ho/tests/test_criterion.py
+++ sparse_ho/tests/test_criterion.py
@@ -1,81 +1,94 @@
+import pytest
+import itertools
 import numpy as np
-import sklearn
-import sklearn.linear_model
-from sklearn.model_selection import KFold
-from sklearn.linear_model import LassoCV
+from sklearn import linear_model
+import celer
 from celer.datasets import make_correlated_data
 
-from sparse_ho.models import Lasso
-from sparse_ho.criterion import CrossVal, HeldOutMSE
+from sparse_ho.models import Lasso, ElasticNet, WeightedLasso, SparseLogreg
+from sparse_ho.criterion import (
+    HeldOutMSE, HeldOutLogistic, FiniteDiffMonteCarloSure)
 from sparse_ho.utils import Monitor
 from sparse_ho import Forward
-from sparse_ho.grid_search import grid_search
 
-n_samples = 100
-n_features = 10
-snr = 3
-corr = 0.5
-
+# Generate data
+n_samples, n_features = 10, 10
 X, y, _ = make_correlated_data(
-    n_samples, n_features, corr=corr, snr=snr, random_state=42)
+    n_samples, n_features, corr=0.1, snr=3, random_state=42)
+sigma_star = 0.1
 
-alpha_max = (np.abs(X.T @ y)).max() / n_samples
-tol = 1e-8
+y = np.sign(y)
+idx_train = np.arange(0, n_samples//2)
+idx_val = np.arange(n_samples//2, n_features)
 
-estimator = sklearn.linear_model.Lasso(
-    fit_intercept=False, max_iter=10000, warm_start=True)
-model = Lasso(estimator=estimator)
+# Set alpha for the Lasso
+alpha_max = (np.abs(X[idx_train, :].T @ y[idx_train])).max() / n_samples
+p_alpha = 0.8
+alpha = p_alpha * alpha_max
+log_alpha = np.log(alpha)
+log_alpha_max = np.log(alpha_max)
+tol = 1e-15
 
-log_alphas = np.log(np.geomspace(alpha_max, alpha_max / 100))
+# Set alpha1 alpha2 for the enet
+alpha_1 = p_alpha * alpha_max
+alpha_2 = 0.1
+log_alpha1 = np.log(alpha_1)
+log_alpha2 = np.log(alpha_2)
+
+dict_log_alpha = {}
+dict_log_alpha["lasso"] = log_alpha
+dict_log_alpha["enet"] = np.array([log_alpha1, log_alpha2])
+tab = np.linspace(1, 1000, n_features)
+dict_log_alpha["wLasso"] = log_alpha + np.log(tab / tab.max())
+dict_log_alpha["logreg"] = (log_alpha - np.log(2))
+
+# Set models to be tested
+models = {}
+models["lasso"] = Lasso(estimator=None)
+models["enet"] = ElasticNet(estimator=None)
+models["wLasso"] = WeightedLasso(estimator=None)
+models["logreg"] = SparseLogreg(estimator=None)
+
+custom_models = {}
+custom_models["lasso"] = Lasso(estimator=celer.Lasso(
+    warm_start=True, fit_intercept=False))
+custom_models["enet"] = ElasticNet(
+    estimator=linear_model.ElasticNet(warm_start=True, fit_intercept=False))
+custom_models["logreg"] = SparseLogreg(
+    estimator=celer.LogisticRegression(warm_start=True, fit_intercept=False))
 
 
-def test_cross_val_criterion():
-    # TODO we also need to add a test for sparse matrices
-    alpha_min = alpha_max / 10
-    log_alpha_max = np.log(alpha_max)
-    log_alpha_min = np.log(alpha_min)
-    max_iter = 10000
-    n_alphas = 10
-    kf = KFold(n_splits=5, shuffle=True, random_state=56)
+# log alpha to be tested by checkgrad
+dict_list_log_alphas = {}
+dict_list_log_alphas["lasso"] = np.log(
+    np.geomspace(alpha_max/2, alpha_max/5, num=5))
+dict_list_log_alphas["wLasso"] = [
+    log_alpha * np.ones(n_features) for log_alpha in
+    dict_list_log_alphas["lasso"]]
+dict_list_log_alphas["logreg"] = np.log(
+    np.geomspace(alpha_max/5, alpha_max/40, num=5))
+dict_list_log_alphas["enet"] = [np.array(i) for i in itertools.product(
+    dict_list_log_alphas["lasso"], dict_list_log_alphas["lasso"])]
 
-    monitor_grid = Monitor()
-    mse = HeldOutMSE(None, None)
-    criterion = CrossVal(mse, cv=kf)
+list_model_crit = [
+    ('lasso',  HeldOutMSE(idx_train, idx_val)),
+    ('enet', HeldOutMSE(idx_train, idx_val)),
+    ('wLasso', HeldOutMSE(idx_train, idx_val)),
+    ('lasso', FiniteDiffMonteCarloSure(sigma_star)),
+    ('logreg', HeldOutLogistic(idx_train, idx_val))]
+
+
+@pytest.mark.parametrize('model_name,criterion', list_model_crit)
+def test_cross_val_criterion(model_name, criterion):
+    # TODO list_criterions = [...]
+    # test val from get_val_grad === get_val
+    # verify dtype from criterion, bonne shape
     algo = Forward()
-    grid_search(
-        algo, criterion, model, X, y, log_alpha_min, log_alpha_max,
-        monitor_grid, max_evals=n_alphas)
-
-    reg = LassoCV(
-        cv=kf, verbose=True, tol=tol, fit_intercept=False,
-        alphas=np.geomspace(alpha_max, alpha_min, num=n_alphas),
-        max_iter=max_iter).fit(X, y)
-    reg.score(X, y)
-    objs_grid_sk = reg.mse_path_.mean(axis=1)
-    # these 2 value should be the same
-    (objs_grid_sk - np.array(monitor_grid.objs))
-    assert np.allclose(objs_grid_sk, monitor_grid.objs)
-
-
-if __name__ == '__main__':
-    test_cross_val_criterion()
-
-
-# TODO list_criterions = [...]
-# test val from get_val_grad === get_val
-# verify dtype from criterion, bonne shape
-
-
-def test_cross_val_criterion():
-    kf = KFold(n_splits=5, shuffle=True, random_state=56)
-    mse = HeldOutMSE(None, None)
-    criterion = CrossVal(mse, cv=kf)
-    algo = Forward()
-
     monitor_get_val = Monitor()
     monitor_get_val_grad = Monitor()
 
-    for log_alpha in log_alphas:
+    model = models[model_name]
+    for log_alpha in dict_list_log_alphas[model_name]:
         criterion.get_val(
             model, X, y, log_alpha, tol=tol, monitor=monitor_get_val)
         criterion.get_val_grad(
@@ -89,4 +102,5 @@ def test_cross_val_criterion():
 
 
 if __name__ == '__main__':
-    test_cross_val_criterion()
+    for model_name, criterion in list_model_crit:
+        test_cross_val_criterion(model_name, criterion)
--- sparse_ho/tests/test_grid_search.py
+++ sparse_ho/tests/test_grid_search.py
@@ -1,11 +1,15 @@
+import pytest
 import numpy as np
 from sklearn import linear_model
+from sklearn.model_selection import KFold
+import celer
 from celer.datasets import make_correlated_data
 
 from sparse_ho.utils import Monitor
-from sparse_ho.models import Lasso
+from sparse_ho.models import Lasso  # , ElasticNet, SparseLogreg
 from sparse_ho import Forward
-from sparse_ho.criterion import HeldOutMSE, FiniteDiffMonteCarloSure
+from sparse_ho.criterion import (
+    HeldOutMSE, FiniteDiffMonteCarloSure, CrossVal, HeldOutLogistic)
 from sparse_ho.grid_search import grid_search
 
 
@@ -17,6 +21,8 @@ corr = 0.5
 X, y, _ = make_correlated_data(
     n_samples, n_features, corr=corr, snr=snr, random_state=42)
 sigma_star = 0.1
+y = np.sign(y)
+
 
 idx_train = np.arange(0, 50)
 idx_val = np.arange(50, 100)
@@ -24,14 +30,73 @@ idx_val = np.arange(50, 100)
 alpha_max = np.max(np.abs(X[idx_train, :].T @ y[idx_train])) / len(idx_train)
 
 log_alphas = np.log(alpha_max * np.geomspace(1, 0.1))
-
 log_alpha_max = np.log(alpha_max)
 log_alpha_min = np.log(0.0001 * alpha_max)
 
 estimator = linear_model.Lasso(
-    fit_intercept=False, max_iter=1000, warm_start=True)
+    fit_intercept=False, max_iter=10000, warm_start=True)
+model = Lasso(estimator=estimator)
+
+tol = 1e-8
+
+# Set models to be tested
+models = {}
+models["lasso"] = Lasso(estimator=None)
+# models["enet"] = ElasticNet(estimator=None)
+# models["logreg"] = SparseLogreg(estimator=None)
+
+models["lasso_custom"] = Lasso(estimator=celer.Lasso(
+    warm_start=True, fit_intercept=False))
+# custom_models["enet"] = ElasticNet(
+#     estimator=linear_model.ElasticNet(warm_start=True, fit_intercept=False))
+# models["logreg_custom"] = SparseLogreg(
+#     estimator=celer.LogisticRegression(warm_start=True, fit_intercept=False))
 
 
+@pytest.mark.parametrize('model_name', list(models.keys()))
+def test_cross_val_criterion(model_name):
+    # TODO we also need to add a test for sparse matrices
+    model = models[model_name]
+    alpha_min = alpha_max / 10
+    log_alpha_max = np.log(alpha_max)
+    log_alpha_min = np.log(alpha_min)
+    max_iter = 10000
+    n_alphas = 10
+    kf = KFold(n_splits=5, shuffle=True, random_state=56)
+
+    monitor_grid = Monitor()
+    if model_name.startswith("lasso"):
+        sub_crit = HeldOutMSE(None, None)
+    else:
+        sub_crit = HeldOutLogistic(None, None)
+    criterion = CrossVal(sub_crit, cv=kf)
+    algo = Forward()
+    grid_search(
+        algo, criterion, model, X, y, log_alpha_min, log_alpha_max,
+        monitor_grid, max_evals=n_alphas, tol=tol)
+
+    if model_name.startswith("lasso"):
+        reg = linear_model.LassoCV(
+            cv=kf, verbose=True, tol=tol, fit_intercept=False,
+            alphas=np.geomspace(alpha_max, alpha_min, num=n_alphas),
+            max_iter=max_iter).fit(X, y)
+    else:
+        reg = linear_model.LogisticRegressionCV(
+            cv=kf, verbose=True, tol=tol, fit_intercept=False,
+            Cs=len(idx_train) / np.geomspace(
+                alpha_max, alpha_min, num=n_alphas),
+            max_iter=max_iter, penalty='l1', solver='liblinear').fit(X, y)
+    reg.score(X, y)
+    if model_name.startswith("lasso"):
+        objs_grid_sk = reg.mse_path_.mean(axis=1)
+    else:
+        objs_grid_sk = reg.scores_[1.0].mean(axis=1)
+    # these 2 value should be the same
+    (objs_grid_sk - np.array(monitor_grid.objs))
+    np.testing.assert_allclose(objs_grid_sk, monitor_grid.objs)
+
+
+# TOD0 factorize this tests
 def test_grid_search():
     max_evals = 5
 
@@ -52,10 +117,10 @@ def test_grid_search():
         monitor_random,
         max_evals=max_evals, tol=1e-5, samp="random")
 
-    assert(monitor_random.log_alphas[
-        np.argmin(monitor_random.objs)] == log_alpha_opt_random)
-    assert(monitor_grid.log_alphas[
-        np.argmin(monitor_grid.objs)] == log_alpha_opt_grid)
+    np.testing.assert_allclose(monitor_random.log_alphas[
+        np.argmin(monitor_random.objs)], log_alpha_opt_random)
+    np.testing.assert_allclose(monitor_grid.log_alphas[
+        np.argmin(monitor_grid.objs)], log_alpha_opt_grid)
 
     monitor_grid = Monitor()
     model = Lasso(estimator=estimator)
@@ -75,11 +140,13 @@ def test_grid_search():
         monitor_random,
         max_evals=max_evals, tol=1e-5, samp="random")
 
-    assert(monitor_random.log_alphas[
-        np.argmin(monitor_random.objs)] == log_alpha_opt_random)
-    assert(monitor_grid.log_alphas[
-        np.argmin(monitor_grid.objs)] == log_alpha_opt_grid)
+    np.testing.assert_allclose(monitor_random.log_alphas[
+        np.argmin(monitor_random.objs)], log_alpha_opt_random)
+    np.testing.assert_allclose(monitor_grid.log_alphas[
+        np.argmin(monitor_grid.objs)], log_alpha_opt_grid)
 
 
 if __name__ == '__main__':
-    test_grid_search()
+    for model_name in models.keys():
+        test_cross_val_criterion(model_name)
+    # test_grid_search()
