92319dea8ef47ad464c58c7382d4cccfca043194
Klopfe <quentin.klopfenstein@u-bourgogne.fr>
2021-01-27 17:28:02 +0100

    Fixed init

--- sparse_ho/models/__init__.py
+++ sparse_ho/models/__init__.py
@@ -2,7 +2,6 @@ from sparse_ho.models.lasso import Lasso
 from sparse_ho.models.enet import ElasticNet
 from sparse_ho.models.svm import SVM
 from sparse_ho.models.svr import SVR
-from sparse_ho.models.ssvr import SSVR
 from sparse_ho.models.wlasso import WeightedLasso
 from sparse_ho.models.logreg import SparseLogreg
 
@@ -10,6 +9,5 @@ __all__ = ['Lasso',
            'ElasticNet',
            'SVM',
            'SVR',
-           'SSVR',
            'WeightedLasso',
            'SparseLogreg']
