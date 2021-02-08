45b2fba6d0e01c901319503d87753d990693fb90
Klopfe <quentin.klopfenstein@u-bourgogne.fr>
2021-02-08 12:10:43 +0100

    fixed fontsize and figsize

--- expes/paths/figure_paths.py
+++ expes/paths/figure_paths.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 
 from sparse_ho.utils_plot import configure_plt
 configure_plt()
+fontsize = 18
 current_palette = sns.color_palette("colorblind")
 
 dict_title = {}
@@ -16,7 +17,8 @@ model_names = ["lasso", "enet", "logreg"]
 plt.close('all')
 fig, axarr = plt.subplots(
     1, len(model_names), sharex=True, sharey=False,
-    figsize=[14, 4], constrained_layout=True)
+    figsize=[10.67, 3.5], constrained_layout=True)
+# check
 # loading results
 for i, model_name in enumerate(model_names):
     coefs = np.load("results/coefs_%s.npy" % model_name, allow_pickle=True)
@@ -31,10 +33,10 @@ for i, model_name in enumerate(model_names):
     for j in range(n_features):
         axarr[i].plot(neg_log_alphas, coefs[:, j], color=current_palette[j])
 
-    axarr[i].set_title(dict_title[model_name])
-    axarr[i].set_xlabel(r"$\lambda_{\max} - \lambda$")
+    axarr[i].set_title(dict_title[model_name], fontsize=fontsize)
+    axarr[i].set_xlabel(r"$\lambda_{\max} - \lambda$", fontsize=fontsize)
 
-axarr[0].set_ylabel("Coefficients")
+axarr[0].set_ylabel("Coefficients value", fontsize=fontsize)
 save_fig = True
 
 if save_fig:
