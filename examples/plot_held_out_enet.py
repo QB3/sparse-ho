8310bd3030e69180ea75fbbc43673a4e2e1b4117
mathurinm <mathurin.massias@gmail.com>
2021-03-26 16:28:01 +0100

    no max_iter in examples

--- examples/plot_held_out_enet.py
+++ examples/plot_held_out_enet.py
@@ -96,7 +96,7 @@ t_grad_search = - time.time()
 monitor = Monitor()
 n_outer = 25
 alpha0 = np.array([alpha_max * 0.3, alpha_max / 10])
-model = ElasticNet(max_iter=max_iter, estimator=estimator)
+model = ElasticNet(estimator=estimator)
 criterion = HeldOutMSE(idx_train, idx_val)
 algo = ImplicitForward(tol_jac=1e-3, n_iter_jac=100, max_iter=max_iter)
 optimizer = GradientDescent(
