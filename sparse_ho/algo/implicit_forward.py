bf33ea9c80652cfcf57fce4c1b8bc731cfc03d08
mathurinm <mathurin.massias@gmail.com>
2021-03-30 14:58:00 +0200

    update get_v description

--- sparse_ho/algo/implicit_forward.py
+++ sparse_ho/algo/implicit_forward.py
@@ -53,8 +53,7 @@ class ImplicitForward():
         model:  instance of ``sparse_ho.base.BaseModel``
             A model that follows the sparse_ho API.
         get_v: callable
-            Function which return the values of the vector v.
-            v is the gradient of the outer criterion.
+            Function which returns the gradient of the outer criterion.
         mask0: ndarray, shape (n_features,)
             Boolean of active feature of the previous regression coefficients
             beta for warm start.
