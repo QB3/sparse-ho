1b9466fefe24f8765a3c16a95082348e787583ac
mathurinm <mathurin.massias@gmail.com>
2021-03-31 10:14:36 +0200

    Cleanup utils_datasets

--- sparse_ho/datasets/utils_datasets.py
+++ sparse_ho/datasets/utils_datasets.py
@@ -3,42 +3,25 @@ from numpy.linalg import norm
 
 import scipy
 
-from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 
 import pandas as pd
 
 
-def get_splits(
-        X, y,
-        sss1=StratifiedShuffleSplit(
-            n_splits=2, test_size=0.3333, random_state=0),
-        sss2=StratifiedShuffleSplit(
-            n_splits=2, test_size=0.5, random_state=0)):
-    for i, (idx_train, idx_test) in enumerate(sss1.split(X, y)):
-        # _ = X[idx_test, :]
-        # y_test = y[idx_test]
-        X_train = X[idx_train, :]
-        y_train = y[idx_train]
-        if i == 0:
-            break
+def get_splits(X, y, train_size=0.333):
+    """TODO"""
 
-    for i, (idx_train, idx_val) in enumerate(sss2.split(X_train, y_train)):
-        # X_val = X_train[idx_val, :]
-        # y_val = y[idx_val]
-        # X_train = X_train[idx_train, :]
-        # y_train = y[idx_train]
-        if i == 0:
-            break
+    idx_train, idx = train_test_split(
+        np.arange(len(y)), stratify=y, train_size=train_size)
 
-    # X_train = X_train.tocsc()
-    # X_val = X_val.tocsc()
-    # X_test = X_test.tocsc()
+    idx_val, idx_test = train_test_split(idx, stratify=y[idx], test_size=0.5)
 
     return idx_train, idx_val, idx_test
 
 
 def clean_dataset(X, y, n_samples, n_features, seed=0):
+    """TODO"""
     np.random.seed(seed)
     idx = np.random.choice(
         X.shape[0], min(n_samples, X.shape[0]), replace=False)
@@ -66,21 +49,13 @@ def clean_dataset(X, y, n_samples, n_features, seed=0):
     X = X[bool_to_keep, :]
     y = y[bool_to_keep]
 
-    # ypd = pd.DataFrame(y)
-    # bool_to_keep = ypd.groupby(0)[0].transform(len) > 2
-    # ypd = ypd[bool_to_keep]
-    # X = X[bool_to_keep.to_numpy(), :]
-    # y = y[bool_to_keep.to_numpy()]
-
     return X, y
 
 
-def get_alpha_max(X, y):
+def alpha_max_multiclass(X, y):
+    """TODO"""
     ypd = pd.DataFrame(y)
-    enc = OneHotEncoder(sparse=False)
-    one_hot_code = enc.fit_transform(ypd)
-
-    one_hot_code = enc.fit_transform(ypd)
+    one_hot_code = OneHotEncoder(sparse=False).fit_transform(ypd)
     n_classes = one_hot_code.shape[1]
 
     alpha_max = np.infty
@@ -88,5 +63,5 @@ def get_alpha_max(X, y):
         alpha_max = min(alpha_max, norm(
             X.T @ (2 * one_hot_code[:, k] - 1), ord=np.inf) / (2 * X.shape[0]))
         if alpha_max == 0:
-            1 / 0
+            raise ValueError("X and y are uncorrelated")
     return alpha_max, n_classes
