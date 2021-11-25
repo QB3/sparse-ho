e41f25ec2e6d7297795d5c6857a40e3ef09fd14b
mathurinm <mathurin.massias@gmail.com>
2021-11-25 21:30:55 +0100

    rm if name == main

--- examples/plot_meg_example.py
+++ examples/plot_meg_example.py
@@ -134,37 +134,36 @@ def solver(
     return X, active_set, monitor
 
 
-if __name__ == '__main__':
-    data_path = sample.data_path()
-    fwd_fname = data_path + '/MEG/sample/sample_audvis-meg-eeg-oct-6-fwd.fif'
-    ave_fname = data_path + '/MEG/sample/sample_audvis-ave.fif'
-    cov_fname = data_path + '/MEG/sample/sample_audvis-shrunk-cov.fif'
-    subjects_dir = data_path + '/subjects'
-    # condition = 'Right Auditory'
-    condition = 'Left Auditory'
+data_path = sample.data_path()
+fwd_fname = data_path + '/MEG/sample/sample_audvis-meg-eeg-oct-6-fwd.fif'
+ave_fname = data_path + '/MEG/sample/sample_audvis-ave.fif'
+cov_fname = data_path + '/MEG/sample/sample_audvis-shrunk-cov.fif'
+subjects_dir = data_path + '/subjects'
+# condition = 'Right Auditory'
+condition = 'Left Auditory'
 
-    if condition == 'Left Auditory':
-        tmax = 0.18
-    else:
-        tmax = 0.15
+if condition == 'Left Auditory':
+    tmax = 0.18
+else:
+    tmax = 0.15
 
-    # Read noise covariance matrix
-    noise_cov = mne.read_cov(cov_fname)
-    # Handling average file
-    evoked = mne.read_evokeds(
-        ave_fname, condition=condition, baseline=(None, 0))
-    evoked.crop(tmin=0.04, tmax=0.18)
+# Read noise covariance matrix
+noise_cov = mne.read_cov(cov_fname)
+# Handling average file
+evoked = mne.read_evokeds(
+    ave_fname, condition=condition, baseline=(None, 0))
+evoked.crop(tmin=0.04, tmax=0.18)
 
-    evoked = evoked.pick_types(eeg=False, meg=True)
-    # Handling forward solution
-    forward = mne.read_forward_solution(fwd_fname)
+evoked = evoked.pick_types(eeg=False, meg=True)
+# Handling forward solution
+forward = mne.read_forward_solution(fwd_fname)
 
-    loose, depth = 0., .8  # corresponds to free orientation
-    stc_wlasso, monitor = apply_solver(
-        evoked, forward, noise_cov, loose, depth, model="wlasso")
-    print("Value of objectives:")
-    print(monitor.objs)
-    ###########################################################################
-    # View in 2D and 3D ("glass" brain like 3D plot)
-    plot_sparse_source_estimates(
-        forward['src'], stc_wlasso, bgcolor=(1, 1, 1), opacity=0.1)
+loose, depth = 0., .8  # corresponds to free orientation
+stc_wlasso, monitor = apply_solver(
+    evoked, forward, noise_cov, loose, depth, model="wlasso")
+print("Value of objectives:")
+print(monitor.objs)
+###########################################################################
+# View in 2D and 3D ("glass" brain like 3D plot)
+plot_sparse_source_estimates(
+    forward['src'], stc_wlasso, bgcolor=(1, 1, 1), opacity=0.1)
