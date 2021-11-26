283bba165640197f90f888f5790a835edf7d534f
QB3 <quentin.bertrand@inria.fr>
2021-11-26 14:41:35 -0500

    [ci skip] more cleaning

--- examples/plot_meg_lasso_vs_wlasso.py
+++ examples/plot_meg_lasso_vs_wlasso.py
@@ -3,7 +3,10 @@
 Weighted Lasso versus Lasso on MEG
 ==================================
 
-This example compares the weighted lasso to the Lasso
+This example compares the Lasso and Weighted Lasso on real MEG data.
+While the the bias of the Lasso leads to optimal coefficients with a lot of
+sources in the brain, the weighted Lasso is able to recover 1 source per
+hemisphere in the brain.
 """
 
 # Authors: Mathurin Massias <mathurin.massas@gmail.com>
@@ -11,36 +14,10 @@ This example compares the weighted lasso to the Lasso
 # License: BSD (3-clause)
 
 import mne
-# from surfer import Brain
-# from mayavi import mlab
 from mne.viz import plot_sparse_source_estimates
 from mne.datasets import sample
 
 from meg_utils import apply_solver
-
-
-# def plot_blob(
-#         stc, subject="sample", surface="inflated", s=14,
-#         data_path=sample.data_path(), subject_name='/subjects',
-#         figsize=(800, 800), event_id=1):
-
-#     subjects_dir = data_path + subject_name
-#     list_hemi = ["lh", "rh"]
-
-#     for i, hemi in enumerate(list_hemi):
-#         figure = mlab.figure(size=figsize)
-#         brain = Brain(
-#             subject, hemi, surface, subjects_dir=subjects_dir,
-#             offscreen=False, figure=figure)
-#         surf = brain.geo[hemi]
-#         sources_h = stc.vertices[i]  # 0 for lh, 1 for rh
-#         for sources in sources_h:
-#             mlab.points3d(
-#                 surf.x[sources], surf.y[sources],
-#                 surf.z[sources], color=(1, 0, 0),
-#                 scale_factor=s, opacity=1., transparent=True)
-#             figure = mlab.gcf()
-#             mlab.close(figure)
 
 
 data_path = sample.data_path()
@@ -48,7 +25,6 @@ fwd_fname = data_path + '/MEG/sample/sample_audvis-meg-eeg-oct-6-fwd.fif'
 ave_fname = data_path + '/MEG/sample/sample_audvis-ave.fif'
 cov_fname = data_path + '/MEG/sample/sample_audvis-shrunk-cov.fif'
 subjects_dir = data_path + '/subjects'
-# condition = 'Right Auditory'
 condition = 'Left Auditory'
 
 if condition == 'Left Auditory':
@@ -74,8 +50,6 @@ for model_name in models:
         evoked, forward, noise_cov, loose, depth, model_name=model_name)
     print("Value of objectives:")
     print(monitor.objs)
-
-    # plot_blob(stc)
 
     ###########################################################################
     # View in 2D and 3D ("glass" brain like 3D plot)
